93f8ff4944e22d6fec2bc337d6598e6bdb50510a
Masahiro Nagano <kazeburo@gmail.com>
2010-11-14 00:53:44 +0900

    initial commit

---
+++ lib/JavaScript/Value/Escape.pm
@@ -0,0 +1,32 @@
+package JavaScript::Value::Escape;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+JavaScript::Value::Escape -
+
+=head1 SYNOPSIS
+
+  use JavaScript::Value::Escape;
+
+=head1 DESCRIPTION
+
+JavaScript::Value::Escape is
+
+=head1 AUTHOR
+
+Masahiro Nagano E<lt>kazeburo {at} gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
